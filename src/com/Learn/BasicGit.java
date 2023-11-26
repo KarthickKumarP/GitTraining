9c09337898a62260e990950c20ab740cb2163355
KarthickKumarP <karthickkp1239@gmail.com>
2023-11-26 07:43:58 +0530

    Maste changes

--- src/com/Learn/BasicGit.java
+++ src/com/Learn/BasicGit.java
@@ -3,7 +3,8 @@ package com.Learn;
 public class BasicGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Project Created...");
 
 	}
 
